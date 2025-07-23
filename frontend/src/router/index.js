46c3d783d8456ae673873ea287feae4febf9b849
Elia <51531784+EliaPfl@users.noreply.github.com>
2025-07-23 12:47:07 +0200

    add backend connection - improvements upload

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -18,7 +18,7 @@ const router = createRouter({
       component: CausalGraphView,
     },
     {
-      path: "/transition",
+      path: "/transition/:ID",
       name: "DomainTransitionGraph",
       component: DomainTransitionGraphView,
     },
