049fa4f8a9fa04f3f92d16fb6cd224ed53445e39
Elia <51531784+EliaPfl@users.noreply.github.com>
2025-08-10 15:23:40 +0200

    Add togglelist CausalGraph  + security updates

--- backend/run.sh
+++ backend/run.sh
@@ -2,4 +2,4 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-.venv/bin/python -m uvicorn --app-dir backend main:app #--reload
+exec .venv/bin/python -m uvicorn --app-dir backend main:app #--reload
